0954489b3efdc76eef1689d3188e7a56563cac40
josealves380 <netopereira464@gmail.com>
2021-02-13 20:11:43 -0300

    Criando componentes de formulario, lista e header

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+        <h2>Nossa Historia</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+        <h2>Missão e visão</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
     </div>
 )
