85b5c7744130eef6ca4082d8f12c3029f192d9a4
josealves380 <netopereira464@gmail.com>
2021-02-15 14:57:54 -0300

    Esconder os botões retorno e exclusão

--- src/todo/todoList.jsx
+++ src/todo/todoList.jsx
@@ -9,11 +9,11 @@ export default props => {
             <tr key={todo._id}>
                 <td className={todo.done ? 'markedAsDone' : ''}>{todo.description}</td>
                 <td>
-                    <IconButton style='success' icon='check'
+                    <IconButton style='success' icon='check' hide={todo.done}
                         onClick={() => props.handleMarkAsDone(todo)}></IconButton>
-                    <IconButton style='warning' icon='undo'
+                    <IconButton style='warning' icon='undo' hide={!todo.done}
                         onClick={() => props.handleMarkAsPending(todo)}></IconButton>
-                    <IconButton style='danger' icon='trash-o'
+                    <IconButton style='danger' icon='trash-o' hide={!todo.done}
                         onClick={() => props.handleRemove(todo)}></IconButton>
                 </td>
             </tr>
