34cc35f4e5fafb1ad9e503cc59d1e992b2b053c1
josealves380 <netopereira464@gmail.com>
2021-02-15 14:52:09 -0300

    Adicionando botões para marcar como concluido e desfazer marcação e funções para controlar

--- src/todo/todoList.jsx
+++ src/todo/todoList.jsx
@@ -7,8 +7,12 @@ export default props => {
         const list = props.list || []
         return list.map(todo => (
             <tr key={todo._id}>
-                <td>{todo.description}</td>
+                <td className={todo.done ? 'markedAsDone' : ''}>{todo.description}</td>
                 <td>
+                    <IconButton style='success' icon='check'
+                        onClick={() => props.handleMarkAsDone(todo)}></IconButton>
+                    <IconButton style='warning' icon='undo'
+                        onClick={() => props.handleMarkAsPending(todo)}></IconButton>
                     <IconButton style='danger' icon='trash-o'
                         onClick={() => props.handleRemove(todo)}></IconButton>
                 </td>
