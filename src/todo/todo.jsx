864e3cd3a9ca86a7b746a5a9789a45f43d40e5ad
josealves380 <netopereira464@gmail.com>
2021-02-14 22:31:05 -0300

    Adicionando controle de estado para formulario

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -5,18 +5,25 @@ import TodoList from './todoList'
 
 export default class Todo extends Component {
     constructor(props) {
-        super(props) 
+        super(props)
+        this.state = { description: '', list: [] }
         this.handleAdd = this.handleAdd.bind(this)
     }
 
+    handleChange(e) {
+        this.setState({...this.state, description: e.target.value })
+    }
+
     handleAdd() {
-        console.log(this)
+
     }
     render() {
         return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
-                <TodoForm handleAdd={this.handleAdd} />
+                <TodoForm description={this.state.description}
+                    handleChange={this.handleChange}
+                    handleAdd={this.handleAdd} />
                 <TodoList />
             </div>
         )
